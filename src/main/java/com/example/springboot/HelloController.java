f4ae6cbb226cefd5f4cca0ba32be7079f6ac3d2d
jimbasler-pivotal <jbasler@vmware.com>
2023-05-14 22:16:31 -0500

    Update welcome message.

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu at OReilly";
+		return "Greetings from Tanzu";
 	}
 
 }
